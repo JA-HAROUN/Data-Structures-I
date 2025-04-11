3064aaff97dd8f0e842f3518edf94e02d82d472b
JA_Billion <youssefatefmohamed25@gmail.com>
2025-04-11 17:16:38 +0200

    beginning in the main part code

--- Lab 4/Lab 4/src/Main.java
+++ Lab 4/Lab 4/src/Main.java
@@ -7,7 +7,52 @@ import java.util.regex.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println( "Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. the list
+        String list = scanner.nextLine();
+
+        // 2. order
+        String order = scanner.nextLine();
+
+        // 3. check which order
+
+        // add to the end
+        if (order.equals("add")){
+            String element = scanner.nextLine();
+            
+            // put the elements to the list (single linked list) [use regex]
+
+        }
+
+        // add to the index
+        else if (order.equals("addToIndex")){}
+
+        // get element in specific index
+        else if (order.equals("get")){}
+
+        // set element in specific index
+        else if (order.equals("set")){}
+
+        // clear the list
+        else if (order.equals("clear")){}
+
+        // check if the list is empty
+        else if (order.equals("isEmpty")){}
+
+        // remove element in specific index
+        else if (order.equals("remove")){}
+
+        // get the size of the list
+        else if (order.equals("size")){}
+
+        // get sublist
+        else if (order.equals("sublist")){}
+
+        // check if the list contains a specific element
+        else if (order.equals("contains")){}
+        
+        scanner.close();
 
     }
 }
